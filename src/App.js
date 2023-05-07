d0cc7398a87ae9ced6447dc1511ccbc725c6afa0
Utpanna Pradhan <86591327+utpanna-pradhan@users.noreply.github.com>
2023-05-07 23:50:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,10 @@ function App() {
   }
 
   return (
-    <div classNameName="App">
+    <div className="App">
+    <div className="welcome">
+    WelCome to TextUtils Website
+    </div>
       <Router>
         <Navbar title="" about="about_Textutils" mode={mode}  toggleMode={toggleMode}   />
         <Alert alert= {alert} />
